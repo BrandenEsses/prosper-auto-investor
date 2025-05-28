47bed81d0dbb831ece92695bb67af672b67f91c5
BrandenEsses <branden.esses@colorado.edu>
2025-05-28 07:42:46 -0600

    Continued Prosper API integration

--- app.py
+++ app.py
@@ -1,7 +1,60 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
+from config import *
 
-app = Flask(__name__)
+access_token = ""
+refresh_token = ""
+
+
+def get_prosper_token():
+    url = "https://api.prosper.com/v1/security/oauth/token"
+    payload = f"grant_type=password&client_id={PROSPER_CLIENT_ID}&client_secret={PROSPER_CLIENT_SECRET}&username={PROSPER_USERNAME}&password={PROSPER_PASSWORD}"
+    headers = { 'accept': "application/json", 'content-type': "application/x-www-form-urlencoded" }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    response_data = json.loads(response.text)
+    global access_token, refresh_token
+    access_token = response_data["access_token"]
+    refresh_token = response_data["refresh_token"]
+    return response_data
+
+def refresh_prosper_token():
+    url = "https://api.prosper.com/v1/security/oauth/token"
+    payload = f"grant_type=refresh_token&client_id={PROSPER_CLIENT_ID}&client_secret={PROSPER_CLIENT_SECRET}&refresh_token={refresh_token}"
+    headers = { 'accept': "application/json", 'content-type': "application/x-www-form-urlencoded" }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    response_data = json.loads(response.text)
+    return response_data
+
+
+def get_notes():
+    url = "https://api.prosper.com/v1/notes/"
+    headers = { 'authorization': f"bearer {access_token}", 'accept': "application/json", 'timezone' : "America/Denver" }
+    response = requests.request("GET", url, headers=headers)
+    response_data = json.loads(response.text)
+    return response_data
+
+def create_app():
+    app = Flask(__name__)
+    with app.app_context():
+        get_prosper_token()
+    return app
+
+app = create_app()
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
+
+@app.route("/notes")
+def notes():
+    current_notes = get_notes()
+    return render_template("notes.html", data = current_notes)
+
+# @app.route("/refresh")
+# def refresh():
+#     return refresh_prosper_token()
+
+# @app.route("/notes")
+# def listings():
+#     return get_listings()
---
+++ config.py
@@ -0,0 +1,6 @@
+from base64 import b64decode
+
+PROSPER_CLIENT_ID = b64decode("").decode('UTF-8')
+PROSPER_CLIENT_SECRET = b64decode("").decode('UTF-8')
+PROSPER_USERNAME = b64decode("").decode('UTF-8')
+PROSPER_PASSWORD = b64decode("").decode('UTF-8')
